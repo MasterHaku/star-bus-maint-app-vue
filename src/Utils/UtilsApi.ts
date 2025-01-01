e7465f7613676612f1bc432969b1a34fc7b58da0
MasterHaku <girardhoel@gmail.com>
2025-01-01 21:01:36 +0100

    Add started app

---
+++ src/Utils/UtilsApi.ts
@@ -0,0 +1,28 @@
+import axios from "axios";
+
+class UtilsApi{
+    url:string;
+
+    constructor(){
+        this.url = "https://data.explore.star.fr/api/explore/v2.1/catalog/datasets/";
+    }
+
+    async performRequest(endpoint:string, offset:number,limit:number): Promise<string[]> {
+
+
+        let data: string[] = [];
+        const finalurl = this.url+endpoint+"?limit="+limit+"&offset="+offset;
+        try {
+            const response = await axios.get(finalurl);
+            data = response.data; // Stocke les données dans `data`
+
+        } catch (error) {
+         console.log(error);   
+        }
+        return data;
+
+    }
+}
+
+export default new UtilsApi();
+//https://data.explore.star.fr/api/explore/v2.1/catalog/datasets/tco-bus-materiel-vehicules-td/records
